a74ba94ecc5dd129444a84e27eb100e0fc704d46
anu-oss <75632877+anu-oss@users.noreply.github.com>
2021-11-10 13:40:17 +0530

    Update Q3.cpp

--- Labs/Week 9/Q3.cpp
+++ Labs/Week 9/Q3.cpp
@@ -1,76 +1,28 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-void minHeapify(int arr[], int i, int n)
-{
-    int left = 2*i+1;
-    int right = 2*i+2;
-    int minimum = i;
-    if(left < n && arr[left]<arr[minimum])
-    {
-        minimum = left;
-    }
-    if(right < n && arr[right]<arr[minimum])
-    {
-        minimum = right;
-    }
-    if(minimum!=i)
-    {
-        swap(arr[i],arr[minimum]);
-        minHeapify(arr,minimum,n);
-    }
-
-}
-
-void buildHeap(int arr[], int n)
-{
-    for(int i=(n-2)/2;i>=0;i--)
-    {
-        minHeapify(arr,i,n);
-    }
-}
-
-int extractMin(int arr[], int *n)
-{
-    int mini = arr[0];
-    swap(arr[0],arr[(*n)-1]);
-    (*n)--;
-    minHeapify(arr,0,*n);
-    return mini;
-}
-
-void insertHeap(int arr[], int value, int *n)
-{
-    (*n)++;
-    arr[(*n)-1] = value;
-    for(int i=(*n)-1;i!=0&&arr[i]<arr[(i-1)/2];)
-    {
-        swap(arr[i],arr[(i-1)/2]);
-        i = (i-1)/2;
-    }
-}
 
 int main()
 {
-    int ans=0,n;
-    cout << "Enter n : ";
-    cin >> n;
-    int arr[n];
-    cout << "Enter array elements : ";
-    for(int i=0;i<n;i++)
-    {
-        cin >> arr[i];
-    }
-    buildHeap(arr, n);
-   while(true)
-    {
-        if(n==0)
-            break;
-        int a = extractMin(arr, &n);
-        int b = extractMin(arr, &n);
-        int sum = a+b;
-        cout << "sum of "  << a << " and " << b << "is " << sum << endl;
-        ans += sum;
-        insertHeap(arr, sum, &n);
-    }
-    cout << ans;
+  int n;
+  cin>>n;
+  priority_queue<int,vector<int>,greater<int> > prio;
+  for(int i=0;i<n;i++)
+  {
+    int t;
+    cin>>t;
+    prio.push(t);
+  }
+  int t1,t2,count,total=0;
+  do
+  {
+    t1=prio.top();
+    prio.pop();
+    t2=prio.top();
+    prio.pop();
+    count=t1+t2;
+    prio.push(count);
+    total+=count;
+  }while(!prio.empty());
+  cout<<total-count;
+  return 0;
 }
