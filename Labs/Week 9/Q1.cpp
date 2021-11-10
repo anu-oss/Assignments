03a7dea11a4f27aac19698611d9825e2380eea79
anu-oss <75632877+anu-oss@users.noreply.github.com>
2021-11-10 13:39:11 +0530

    Update Q1.cpp

--- Labs/Week 9/Q1.cpp
+++ Labs/Week 9/Q1.cpp
@@ -1,53 +1,53 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-#define V 5
-#define INF 999999
 
-void printMatrix(int graph[][V]);
 
-void FloyedWarshall(int graph[][V])
-{
-    for(int i=0;i<V;i++)
-    {
-        for(int j=0;j<V;j++)
-        {
-            for(int k=0;k<V;k++)
-            {
-                if(graph[i][j] > graph[i][k]+graph[k][j])
-                {
-                    graph[i][j] = graph[i][k]+graph[k][j];
-                }
-            }
-        }
-    }
-    printMatrix(graph);
-}
-
-void printMatrix(int graph[][V])
-{
-    for(int i=0;i<V;i++)
-    {
-        for(int j=0;j<V;j++)
-        {
-            if(graph[i][j] == INF)
-            {
-                cout << "INF ";
-            }
-            else{
-                cout << graph[i][j] << " ";
-            }
-        }
-        cout << endl;
-    }
-}
 
 int main()
 {
-    int graph[V][V] = {{0,10,5,5,INF},
-                       {INF,0,5,5,5},
-                       {INF,INF,0,INF,10},
-                       {INF,INF,INF,0,20},
-                       {INF,INF,INF,5,0}};
+  int V;
+  cin>>V;
+  int **graph = new int*[V];
+  for(int i=0;i<V;i++){
+    graph[i] = new int[V];
+    for(int j=0;j<V;j++)
+      cin>>graph[i][j];
+  }
 
-    FloyedWarshall(graph);
+  for(int i=0;i<V;i++)
+  {
+    for(int j=0;j<V;j++)
+    {
+      cout<<graph[i][j]<<" ";
+    }
+    cout<<"\n";
+  }
+
+  for(int k=0;k<V;k++)
+  {
+    for(int i=0;i<V;i++)
+    {
+      for(int j=0;j<V;j++)
+      {
+        if(graph[i][j]>(graph[i][k]+graph[k][j]) and
+            (graph[k][j] != 99
+            and graph[i][k] != 99))
+          graph[i][j]=graph[i][k]+graph[k][j];
+      }
+    }
+  }
+  
+  for(int i=0;i<V;i++)
+  {
+    for(int j=0;j<V;j++)
+    {
+      cout<<graph[i][j]<<" ";
+    }
+    cout<<"\n";
+  }
+
+
+
+
+  return 0;
 }
